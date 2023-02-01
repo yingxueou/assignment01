c9a4d6f79b199d279b17b308cb57ebfd08fe49c7
Mjumbe Poe <mjumbewu@gmail.com>
2023-02-01 05:06:10 -0500

    Lint the js tests

--- __tests__/test_part1_query01.js
+++ __tests__/test_part1_query01.js
@@ -1,9 +1,9 @@
-import {expect, describe, it} from '@jest/globals';
+import { expect, describe, it } from '@jest/globals';
 import './jest_extensions';
 
 describe('Part 1 - Query 01', () => {
   it('should return the correct results', async () => {
     await expect('../part1/query01.sql')
       .toReturnRecords(['expected_results/part1/query01.csv']);
-  })
-})
+  });
+});
